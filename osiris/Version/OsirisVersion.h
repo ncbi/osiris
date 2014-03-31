b725815e031c9eeacb9d0c5a165d038214e0f876
Douglas Hoffman <hoffman@ncbi.nlm.nih.gov>
2014-03-31 08:33:52 -0400

    beta 6

--- osiris/Version/OsirisVersion.h
+++ osiris/Version/OsirisVersion.h
@@ -37,7 +37,7 @@
 #define OSIRIS_OS "Macintosh"
 #endif
 #endif
-#define OSIRIS_VERS_BASE "2.3 Beta 5"
+#define OSIRIS_VERS_BASE "2.3 Beta 6"
 #define OSIRIS_VERS OSIRIS_VERS_BASE
 //" beta 17"
 #define OSIRIS_VERSION OSIRIS_VERS " for " OSIRIS_OS
