0d47d210d33b72a1c148d6affb391d0e70f864c3
Douglas Hoffman <hoffman@ncbi.nlm.nih.gov>
2015-03-09 10:40:31 -0400

    added 2.4 release notes to the distribution

--- osiris/ZipPerl/MakeZip.pl
+++ osiris/ZipPerl/MakeZip.pl
@@ -146,6 +146,7 @@ sub COPYFILES
   &SYSTEM("${CP} ${src}/docs/readme.rtf ${dest}");
   &SYSTEM("${CP} ${src}/docs/OsirisHelp.pdf ${dest}");
   &SYSTEM("${CP} ${src}/docs/V2.3-Change-Log.pdf ${dest}");
+  &SYSTEM("${CP} ${src}/docs/OSIRIS_Release_Notes_v2.4.pdf ${dest}");
 }
 
 sub CopyWin
