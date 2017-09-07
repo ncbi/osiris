ea2044fb91f74a6a980400cdb358019122da0e7e
Hoffman, Doug <hoffman@ncbi.nlm.nih.gov>
2017-09-07 16:43:52 -0400

    problems when locking/unlocking volumes When locking a volume, it was not be reloaded if modified when changing a volume it was incorrectly stating that changes were made (prompting user to save changes)

--- osiris/OsirisAnalysis/CVolumes.cpp
+++ osiris/OsirisAnalysis/CVolumes.cpp
@@ -191,8 +191,8 @@ bool CVolume::CheckReload(bool bForceReload)
   bool bRtn = false;
   if(!IsOK())
   {} // not previously loaded
-  else if(IsLocked() && !HasLock())
-  {} // someone else has it locked, cannot load
+//  else if(IsLocked() && !HasLock())  // 9/7/16, continue to check even if locked
+//  {} // someone else has it locked, cannot load
   else if(bForceReload)
   {
     bRtn = m_lab.ReloadFile()
@@ -274,6 +274,7 @@ bool CVolume::Lock()
 #endif
   else if(m_lock.Lock(m_sPath))
   {
+    CheckReload();
     bRtn = true;
   }
   else
