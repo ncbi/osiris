74f7f1087448626caa6809bc176834568b14ebd7
hoffman <hoffman@ncbi.nlm.nih.gov>
2019-10-10 09:18:33 -0400

    added debug build scripts

---
+++ osiris/build-osiris-mac-osx-debug.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if test "$WXHOME" = ""; then
+  WXHOME=`sh findwx`
+  if test "$WXHOME" = ""; then
+    echo "Cannot find WXHOME in environment" 1>&2
+    exit 1
+  fi
+  export WXHOME
+fi
+for x in wxthings/lib wxplotctrl/lib BaseClassLib/lib OsirisMath/lib OsirisLib2.01/lib TestAnalysisDirectoryLCv2.11/bin fsa2xml/bin wxXml2/lib wxXsl/lib nwxZip/lib nwx/lib OsirisAnalysis/bin OpenFolder/bin; do
+(cd $x ; pwd ; aclocal; autoconf; automake -a)
+done
+(cd OsirisAnalysis; sh updatePlist.sh)
+(cd OsirisLib2.01 ; sh blex.sh clean all )
+
+aclocal
+autoconf
+automake -a
+cp osiris.m4.debug osiris.m4
+cp osiriscl.m4.debug osiriscl.m4
+./configure --enable-debug --enable-debug_gdb
