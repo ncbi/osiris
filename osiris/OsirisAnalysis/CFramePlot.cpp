38087cd5a4ee7886f4027d6368abea865cb41d1c
Hoffman <hoffman@ncbi.nlm.nih.gov>
2020-08-31 18:02:03 -0400

    small bug fix related to printing

--- osiris/OsirisAnalysis/CFramePlot.cpp
+++ osiris/OsirisAnalysis/CFramePlot.cpp
@@ -2161,7 +2161,10 @@ void CFramePlot::OnSize(wxSizeEvent &e)
 void CFramePlot::_SetupBitmapPlot()
 {
   // called from CFramePlot::_GetBitmapPlot()
-
+  if (m_pPlotForBitmap != NULL)
+  {
+    m_pPlotForBitmap->Destroy();
+  }
   m_pPlotForBitmap = new CPanelPlot(this, m_pData, m_pOARfile, NULL, m_pColors, 0, false, 0, true);   // EXT TIMER , true);
   m_pPlotForBitmap->SetRenderingToWindow(false);
   m_pPlotForBitmap->SetSync(false);
