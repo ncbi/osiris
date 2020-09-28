4a803e50c72cf35264880d8fd5fc35b4b2b590b9
Hoffman <hoffman@ncbi.nlm.nih.gov>
2020-09-28 16:12:39 -0400

    added 2.14 release notes to 2.15 beta

--- osiris/Setup/uninstall.bat
+++ osiris/Setup/uninstall.bat
@@ -1,6 +1,2 @@
 @echo off
-<<<<<<< HEAD
-start %SystemRoot%\system32\MsiExec.exe /x{66E4E628-419A-413E-A87F-D7C4A0574CA7}
-=======
-start %SystemRoot%\system32\MsiExec.exe /x{C4218548-D5E4-4F6D-A042-B6DE2E92963B}
->>>>>>> 7251fc8eac2653b2c362faa6b9e721b21efd569b
+start %SystemRoot%\system32\MsiExec.exe /x{6CDEAA00-2667-4AD6-A6F3-6E36FF772353}
