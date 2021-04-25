663272f9cdc64c4ea3f8201d3d5de97c5880c0fa
Hoffman <hoffman@ncbi.nlm.nih.gov>
2021-04-25 16:36:39 -0400

    Merged Version-2.16-beta into Version-2.16-beta-CopyBinsAndPanels

--- osiris/OsirisAnalysis/CPanelPlot.cpp
+++ osiris/OsirisAnalysis/CPanelPlot.cpp
@@ -2467,10 +2467,6 @@ wxMenuItem *CPanelPlot::GetMenuItem(wxMenu *p)
       "",               // help
       wxITEM_NORMAL,        // wxItemKind
       m_pMenu);             // wxMenu *subMenu
-<<<<<<< HEAD
-    unique_ptr<nwxMenuItem> x(pMenuItem);
-=======
->>>>>>> Version-2.16-beta
     m_pMenuItem.reset(pMenuItem);
   }
   else
--- osiris/Setup/uninstall.bat
+++ osiris/Setup/uninstall.bat
@@ -1,10 +1,2 @@
 @echo off
-<<<<<<< HEAD
-<<<<<<< HEAD
-start %SystemRoot%\system32\MsiExec.exe /x{79438078-FB1B-48C8-BDCB-EEA894239C30}
-=======
-start %SystemRoot%\system32\MsiExec.exe /x{0D4BFB0B-3675-45BD-A52A-01425B0D1EBA}
->>>>>>> Version-2.16
-=======
 start %SystemRoot%\system32\MsiExec.exe /x{13EBCFE7-E5FB-4553-87C9-682FC4228B76}
->>>>>>> Version-2.16-beta
